3252a0b73286b71cee36274e1212bc9bb1460283
satvik aggarwal <skaggarwal2001.com@gmail.com>
2020-10-01 14:24:30 +0530

    manual files

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,26 @@
 // new WOW().init();
+
   function start() {
     document.querySelector('.preloader').style.display = 'none';
     document.querySelector('.afterloader').style.display = 'block';
 
   }
-  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1W8N6vNM_8leqLLZR1EDRnHgqHTXRDy7e_nB6q21wvAw/edit?usp=sharing';
 
-  function init() {
-    Tabletop.init( { key: publicSpreadsheetUrl,
-                     callback: showInfo,
-                     simpleSheet: true } )
-  }
-
-  function showInfo(data, tabletop) {
-    console.log(data);
-  }
-
-  window.addEventListener('DOMContentLoaded', init);
+ document.querySelector('body').onload = start();
+  // var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1W8N6vNM_8leqLLZR1EDRnHgqHTXRDy7e_nB6q21wvAw/edit?usp=sharing';
+  //
+  // function init() {
+  //   Tabletop.init( { key: publicSpreadsheetUrl,
+  //                    callback: showInfo,
+  //                    simpleSheet: true } )
+  // }
+  //
+  // function showInfo(data, tabletop) {
+  //   console.log(data);
+  // }
+  //
+  // window.addEventListener('DOMContentLoaded', init);
+  AOS.init();
 
   function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
