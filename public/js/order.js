5ca5555a914438f44d5ff4471d87f4ff7bfc336d
satvik aggarwal <skaggarwal2001.com@gmail.com>
2020-10-09 10:45:55 +0530

    add product

---
+++ public/js/order.js
@@ -0,0 +1,19 @@
+var modal = document.querySelectorAll('.openmodal');
+for (let i = 0; i < modal.length; i++) {
+    modal[i].addEventListener("click", function() {
+      var myBookId = $(this).data('id');
+      console.log(myBookId);
+      console.log(myBookId.address.pincode);
+      $(".modal-body #orderid").innerHTML =  myBookId ;
+
+    });
+}
+// modal.addEventListener("click" ,function () {
+//      // var myBookId = $(this).data('id');
+//      // $(".modal-body #bookId").val( myBookId );
+//      // As pointed out in comments,
+//      // it is unnecessary to have to manually call the modal.
+//      // $('#addBookDialog').modal('show');
+//
+//
+// });
